a0c509de2badd17e788454009a42765594433e7b
fjrm <fcjavich2@gmail.com>
2023-03-28 12:29:19 +0200

    first commit

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,15 @@
+package holamundo;
+
+/**
+ *
+ * @author Javi
+ */
+
+public class HolaMundo {
+
+    //donde empieza el programa Java
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!!");
+    }
+    
+}
---
+++ src/holamundo/Programa1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package holamundo;
+
+/**
+ *
+ * @author Javi
+ */
+
+public class Programa1 {
+    
+    //donde empieza el programa Java
+    public static void main(String[] args) {
+        byte edad = 30; //valor entre -128 y 127
+        System.out.println("La edad es: " + edad + " años");
+    }
+    
+}
